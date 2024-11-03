1196b6d3213527c75216bbe6e2fe86d7e04f11a5
Sophie713 <sophie713713@gmail.com>
2024-11-04 00:35:55 +0100

    timeout

--- app/src/main/java/com/sophiemiller/newsapp/data/dimodules/NetworkModule.kt
+++ app/src/main/java/com/sophiemiller/newsapp/data/dimodules/NetworkModule.kt
@@ -9,6 +9,7 @@ import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 /**
@@ -25,6 +26,9 @@ object NetworkModule {
     @Singleton
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
             .build()
     }
 

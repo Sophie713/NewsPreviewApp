41b06d3570c17e128c448267c515cc1aea63afd6
Sophie713 <sophie713713@gmail.com>
2024-11-03 19:14:33 +0100

    module + interceptor

---
+++ app/src/main/java/com/sophiemiller/newsapp/data/dimodules/NetworkModule.kt
@@ -0,0 +1,57 @@
+package com.sophiemiller.newsapp.data.dimodules
+
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.sophiemiller.newsapp.BuildConfig
+import com.sophiemiller.newsapp.data.interceptor.AuthInterceptor
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+/**
+ * Dagger/Hilt module for data layer
+ */
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://newsdata.io/"
+
+    @Provides
+    @Singleton
+    fun provideAuthInterceptor(): Interceptor {
+        return AuthInterceptor(BuildConfig.NEWS_API_KEY)
+    }
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(authInterceptor: Interceptor): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(authInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, gson: Gson): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideGson(): Gson {
+        return GsonBuilder().setLenient().create()
+    }
+
+}
